19a1d61eb9980816d8032de520c5b28ae0bcc299
IamZombiez <dereksiu216@gmail.com>
2018-06-25 11:48:37 -0700

    more css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Nav/Toolbar/Toolbar';
 import SideDrawer from '../Nav/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-)
+class Layout extends Component {
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
-
-export default layout;
+export default Layout;
--- src/components/Nav/SideDrawer/SideDrawer.js
+++ src/components/Nav/SideDrawer/SideDrawer.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Logo from '../../Logo/Logo'
 import NavItems from '../NavItems/NavItems'
 import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+
 
 const sideDrawer = (props) => {
 
   return (
+    <Aux>
+    <Backdrop />
     <div className={classes.SideDrawer}>
       <Logo height="11%"/>
       <nav>
         <NavItems />
       </nav>
     </div>
+    </Aux>
   );
 }
 
